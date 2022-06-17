0add28421ec5ef904748276347a7682cf716a4df
liazz577 <liamcritlance@gmail.com>
2022-06-17 20:12:18 +0700

    update

---
+++ src/app/exam/exam.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {WeatherServices} from "../services/weather.services";
+
+@Component({
+  selector: 'app-exam',
+  templateUrl: './exam.component.html',
+  styleUrls: ['./exam.component.css']
+})
+export class ExamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/services/weather.services.ts
@@ -0,0 +1,40 @@
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {IWeather} from "../interface/weather.interface";
+import {Injectable} from "@angular/core";
+import {ICategory, IDataJSON} from "../interface/category.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherServices {
+  static BASE_URL: string = 'https://api.openweathermap.org/data/2.5/';
+  static APPID: string = '09a71427c59d38d6a34f89b47d75975c';
+  constructor(private httpClient: HttpClient) {
+  }
+
+  currentWeather(cityCode: string){
+    var parameters = new HttpParams();
+    parameters = parameters.append('q',cityCode);
+    parameters = parameters.append('appid',WeatherServices.APPID);
+    parameters = parameters.append('units','metric');
+    return this.httpClient.get<IWeather>(WeatherServices.BASE_URL+"weather",
+      {
+        params: parameters
+      })
+  }
+
+  foreCast(cityCode: string){
+    var parameters = new HttpParams();
+    parameters = parameters.append('q',cityCode);
+    parameters = parameters.append('appid',WeatherServices.APPID);
+    parameters = parameters.append('units','metric');
+    return this.httpClient.get<any>(WeatherServices.BASE_URL+"forecast",
+      {
+        params: parameters
+      })
+  }
+  mailList(){
+    return this.httpClient.get<IDataJSON>('http://localhost:4200/assets/data.json');
+  }
+}
+
